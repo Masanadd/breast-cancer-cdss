ec48fa561053e7c39ba56c8cbeec5699db369ce8
Masanadd <155971568+Masanadd@users.noreply.github.com>
2025-03-26 14:21:03 +0100

    Remove hf_login.py and test_hf.py from versioning (now ignored)

--- hf_login.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import login
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-hf_token = os.getenv("HUGGINGFACE_TOKEN")
-
-login(token=hf_token)
--- test_hf.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-import torch
-
-torch.hub.set_dir("./cache")  # Forzar almacenamiento en caché local
-
-print("🔄 Descargando modelo, espera un momento...")
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", cache_folder="./cache")
-print("✅ Modelo descargado correctamente")
